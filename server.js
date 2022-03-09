fcc2c00afcc0d9c1ce57b4b983ec8f48d867b2cf
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-03-09 16:23:45 +0000

    parameterised route with basic error handling

--- server.js
+++ server.js
@@ -4,8 +4,41 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+  {
+    id: 0,
+    name: 'Nikola Tesla',
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton',
+  },
+  {
+    id: 2,
+    name: 'Albert Einstein',
+  },
+];
+
 app.get('/', (req, res) => {
   res.send('Express installed');
+});
+
+app.get('/friends', (req, res) => {
+  // res.send(friends);  << this works, but below is better
+  res.json(friends); // express treats this as JSON data
+});
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = req.params.friendId;
+  // validate
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: 'Friend does not exist',
+    });
+  }
 });
 
 app.get('/html', (req, res) => {
