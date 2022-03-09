593f039fbeca33c717d83b6f62c07652543c4d14
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2022-03-09 15:57:38 +0000

    basic express routes

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Express installed');
+});
+
+app.get('/html', (req, res) => {
+  res.send('<h1>Express Messages Route</h1>');
+});
+
+app.get('/messages', (req, res) => {
+  console.log('Updating messages...');
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
